e901129dbc05b9846c177ee50d3db3ead74d553e
ahhzaky <siahmadzaky@gmail.com>
2023-12-05 13:29:26 +0700

    Update layout register

--- src/pages/@auth/register/layout.jsx
+++ src/pages/@auth/register/layout.jsx
@@ -5,7 +5,7 @@ export default function RegisterLayout({ children }) {
     return (
       <>
         <Head>
-            <title>Register Akun | Investor Mode</title>
+            <title>Register Akun | Campaign Mode</title>
             <meta name="description" content="Silahkan Register sebagai pengusaha Crowdfiunding UMKM"/>
         </Head>
         {children}
